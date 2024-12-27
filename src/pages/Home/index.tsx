ab7b646df6312ce93fafa278d49f06da5896fc7d
Adryel Santos <118002541+AdryelJr@users.noreply.github.com>
2024-12-26 21:27:02 -0300

    up code

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,23 @@
+import { useState, useEffect } from "react";
 import { Footer } from "../components/Footer/Footer";
 import { Header } from "../components/Header/Header";
 import { MainCards } from "../components/Main/Main-Cards/Mais-Cards";
 
 export function Home() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => setIsVisible(true), 800);
+    }, []);
+
     return (
-        <div>
+        <div
+            className={`transform transition-all duration-700 ease-in-out ${isVisible ? "opacity-100 scale-100" : "opacity-0 scale-95"
+                }`}
+        >
             <Header />
-            <h1>Home</h1>
             <MainCards />
             <Footer />
         </div>
-    )
+    );
 }
--- src/pages/components/Main/Main-Cards/Mais-Cards.tsx
+++ src/pages/components/Main/Main-Cards/Mais-Cards.tsx
@@ -2,21 +2,34 @@ import { useEffect, useState } from "react";
 import { Cards } from "../Cards/Cards";
 import { ImovelType } from "@/hooks/types";
 import { GetImoveisDB } from "@/firebase/admin/getDashboard";
+import { Loader2 } from "lucide-react";
 
 export function MainCards() {
     const [dataBD, setDataBD] = useState<ImovelType[]>([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         async function handleGetBD() {
             try {
+                setLoading(true);
                 const data = await GetImoveisDB();
                 setDataBD(data)
             } catch (error) {
-                console.log(error)
+                console.error(error);
+            } finally {
+                setLoading(false);
             }
         }
         handleGetBD()
     }, [])
+
+    if (loading) {
+        return (
+            <div className="flex items-center justify-cente">
+                <Loader2 className="w-8 h-8 animate-spin" />
+            </div>
+        )
+    }
 
     return (
         <div className="flex flex-wrap justify-center">
